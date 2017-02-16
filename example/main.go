e59fe0742060baa9d0452ad0adc5a5454b6b3e1e
athrun <xu_tian_cheng@outlook.com>
2017-02-15 22:36:47 -0500

    make build success

--- example/main.go
+++ example/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/AthrunEcho/zkteco"
+	"github.com/athrunecho/zkteco"
 	"github.com/northbright/pathhelper"
 )
 
@@ -12,9 +12,19 @@ var (
 	p   string
 )
 
+const (
+	kaoqingFilePath = "./in/kaoqing.csv"
+)
+
 func main() {
+	var records = [][]string{}
+
 	if p, err = pathhelper.GetAbsPath(kaoqingFilePath); err != nil {
-		i := zkteco.GetCSVRecords(p)
-		fmt.Printf("%v\n", i)
+		fmt.Printf("GetAbsPatherr:%v\n", err)
+	}
+
+	if records, err = zkteco.GetCSVRecords(p); err != nil {
+		fmt.Printf("records:%v\n, err:%v\n", records, err)
+
 	}
 }
