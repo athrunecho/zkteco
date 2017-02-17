585aaad5a0f848228f7cc8694877f72a9a32094b
athrun <xu_tian_cheng@outlook.com>
2017-02-17 04:02:22 -0500

    Add csv_test.go

---
+++ csv_test.go
@@ -0,0 +1,23 @@
+package zkteco_test
+
+import (
+	"log"
+	"github.com/athrunecho/zkteco"
+)
+
+func ExampleGetCSVRecords() {
+	var err error
+	var 
+	
+        log.Printf("\n")
+	log.Printf("--------- GetCSVRecords() Test Begin --------\n")
+       
+        
+
+end:
+	if err != nil {
+		debugPrintf("errer: %v\n", err)
+	}
+
+	// Output:
+}
