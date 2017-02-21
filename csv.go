24cad808d2d5a5fa1c4fec38c7f6e949e236d8ec
athrun <xu_tian_cheng@outlook.com>
2017-02-21 01:43:41 -0500

    Modified function

--- csv.go
+++ csv.go
@@ -2,21 +2,12 @@ package zkteco
 
 import (
 	"encoding/csv"
-	"os"
+	"io"
 )
 
-func GetCSVRecords(name string) (records [][]string, err error) {
-	var f *os.File
-
-	f, err = os.Open(name)
-	if err != nil {
-		debugPrintf("os.Open() err: %v\n", err)
-		return [][]string{}, err
-	}
-	defer f.Close()
-
-	r := csv.NewReader(f)
-	records, err = r.ReadAll()
+func GetCSVRecords(r io.Reader) (records [][]string, err error) {
+	reader := csv.NewReader(r)
+	records, err = reader.ReadAll()
 	if err != nil {
 		debugPrintf("GetCSVRecords error: %v\n", err)
 		return [][]string{}, err
