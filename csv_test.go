a79bc20c34d3e80f4c8314c4106a75de4094ce96
athrun <xu_tian_cheng@outlook.com>
2017-02-23 03:46:18 -0500

    add csv_test

--- csv_test.go
+++ csv_test.go
@@ -2,22 +2,49 @@ package zkteco_test
 
 import (
 	"log"
+	"os"
+
 	"github.com/athrunecho/zkteco"
+	"github.com/northbright/pathhelper"
 )
 
+const (
+	kaoqinFilePath = "./example/csv/attendance/attendance.csv"
+)
+
+// 1. Run "go test -c && ./zkteco.test" to generate test binary under root of project dir and start test.
+//
+// 2. Run "go test" will generate test binary under "/tmp" with a random test dir
 func ExampleGetCSVRecords() {
-	var err error
-	var 
-	
-        log.Printf("\n")
+	var (
+		err error
+		p   string
+		f   *os.File
+		re  [][]string
+	)
+
+	log.Printf("\n")
 	log.Printf("--------- GetCSVRecords() Test Begin --------\n")
-       
-        
+
+	if p, err = pathhelper.GetAbsPath(kaoqinFilePath); err != nil {
+		goto end
+	}
+
+	if f, err = os.Open(p); err != nil {
+		goto end
+	}
+	defer f.Close()
+
+	if re, err = zkteco.GetCSVRecords(f); err != nil {
+		goto end
+	}
+
+	log.Printf("%v\n", re)
 
 end:
 	if err != nil {
-		debugPrintf("errer: %v\n", err)
+		log.Printf("err: %v\n", err)
 	}
-
+	log.Printf("--------- BatchCreate() Test End --------\n")
 	// Output:
 }
